862e9dba12fd594e64728f0ff6e101c639eda9e9
DESKTOP-9O360Q0\SEATECH <hungvm@seatechit.com.vn>
2023-06-15 17:33:28 +0700

    header

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,19 @@
 window.addEventListener("load", function () {
-  const toggle = document.querySelector(".menu-toggle");
-  const menu = document.querySelector(".menu");
+  const toggle = document.querySelector(".header-toggle");
+  const menu = document.querySelector(".header-menu");
   toggle && toggle.addEventListener("click", handleToggleMenu);
   function handleToggleMenu(e) {
     menu && menu.classList.add("is-show");
   }
   document.addEventListener("click", handleClickOutside);
   function handleClickOutside(e) {
-    if (e.target.matches(".menu-toggle") || e.target.matches(".menu, .menu *")) return;
-    menu && menu.classList.remove("is-show");
+    if (!menu.contains(e.target) && !e.target.matches(".header-toggle")) menu && menu.classList.remove("is-show");
   }
 });
+
+// const menuToggle = document.querySelector(".header-toggle");
+// const menuHeader = document.querySelector(".header-menu");
+// const expandClass = "is-expand";
+// menuToggle.addEventListener("click", function () {
+//   menuHeader.classList.add(expandClass);
+// });
