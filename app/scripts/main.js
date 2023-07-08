d8337f2a1ea714b62338b0081c23fef4f9186e19
hungvm.dev <hungvumanh2001@gmail.com>
2023-07-08 19:25:25 +0700

    slick slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,3 +18,21 @@ window.addEventListener("load", function () {
 // menuToggle.addEventListener("click", function () {
 //   menuHeader.classList.add(expandClass);
 // });
+
+// Slick slider
+$(document).ready(function () {
+  $(".quote-container").slick({
+    prevArrow:
+      "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+});
